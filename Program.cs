0a917478fcf1b4a7befcaab029360867c1f23a70
Tehilah <tehillahk@outlook.com>
2024-06-01 01:34:06 -0500

    linked list works

--- Program.cs
+++ Program.cs
@@ -2,6 +2,9 @@
 
 
 Stack stack = new Stack();
+LinkedList list = new LinkedList();
 var success = stack.push(1);
+var insertion = list.insert(1);
 
 Console.WriteLine(success);
+Console.WriteLine(insertion);
